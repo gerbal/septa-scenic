f17fe7d7fa8d68e4e2543fdafb850c8bb9167c67
Grant McLendon <grant@7mind.de>
2020-10-29 14:08:17 -0500

    try adding releasing with bakeware

--- mix.exs
+++ mix.exs
@@ -1,14 +1,29 @@
 defmodule ScenicSepta.MixProject do
   use Mix.Project
+  @app :scenic_septa
 
   def project do
     [
-      app: :scenic_septa,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.7",
       build_embedded: true,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # releases: release(),
+    ]
+  end
+
+  def release do
+    [
+      scenic_septa: [
+        steps:  [
+          overwrite: true,
+          cookie: "#{@app}_cookie",
+          steps: [:assemble, &Bakeware.assemble/1],
+          strip_beams: Mix.env() == :prod
+        ]
+      ]
     ]
   end
 
@@ -26,6 +41,7 @@ defmodule ScenicSepta.MixProject do
       {:scenic, "~> 0.10"},
       {:scenic_driver_glfw, "~> 0.10", targets: :host},
       {:scenic_clock, "~> 0.10"},
+      # {:bakeware, "~> 0.1", runtime: false},
       # {:scenic_live_reload, "~> 0.2", only: :dev}
     ]
   end
